96edf5df3ca136400fcc0f4d1a724111c122d4b5
JohnPantsWork <johnpantswork@gmail.com>
2021-09-17 19:15:57 +0800

    [Aming] unity gun update

---
+++ UdemyUnityCourse06/Assets/Weapon.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            // hit effect
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            //call enemy health  and decrease
+        }
+        else
+        {
+            return;
+        }
+
+
+    }
+}
